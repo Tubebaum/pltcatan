60efba4f88b421d587a626b26aaa276cbbe4ec99
Andrew FigPope <drewii2ii@gmail.com>
2015-04-08 14:13:31 -0400

    Moved I/O loop into main call, and added parsing hook

--- input_parser/parser.py
+++ input_parser/parser.py
@@ -155,10 +155,14 @@ def p_empty(p):
 
 yacc.yacc()
 
-while 1:
-	try:
-		s = raw_input('>')
-	except EOFError:
-		break
-	if not s: continue
-	yacc.parse(s)
+def parse_string(s):
+	return yacc.parse(s)
+
+if __name__ == '__main__':
+	while 1:
+		try:
+			s = raw_input('>')
+		except EOFError:
+			break
+		if not s: continue
+		yacc.parse(s)
